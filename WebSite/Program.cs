724b0513e960cbb949de0147d405a74996e67953
Ladulllka <lordraduv@gmail.com>
2023-05-30 15:35:13 +0300

    Обновил проекты в решении

--- WebSite/Program.cs
+++ WebSite/Program.cs
@@ -1,19 +1,17 @@
-
-
 using Newtonsoft.Json;
 using WarehouseApi2.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -25,7 +23,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 HttpClient client = new HttpClient();
 HttpResponseMessage response = await client.GetAsync("https://localhost:7188/Show/Stock");
@@ -49,7 +49,5 @@ else
     Console.WriteLine($"Status code: {response.StatusCode}");
     Console.WriteLine($"Error message: {response.ReasonPhrase}");
 }
-
-Console.WriteLine("!");
 
 app.Run();
